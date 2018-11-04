2b76aff0f2ee78711f3ece2e2e8818960eb84916
Silvia Patono <silvia.patono@gmail.com>
2018-11-04 18:27:04 +0100

    Check smile starts with eyes

--- smileyfaces.php
+++ smileyfaces.php
@@ -1,1 +1,12 @@
 <?php
+
+function count_smileys($arr): int {
+
+  	foreach ($arr as $value) {
+		if (substr($value, 0, 1) === ':' | substr($value, 0, 1) === ';') {
+			
+		}
+	}
+}
+
+count_smileys([':D',':~)',';~D',':)']);
